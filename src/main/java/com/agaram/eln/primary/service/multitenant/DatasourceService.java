4b123a10bcc9a978b6532a5139e62662992d3c0a
pradeepmanas <pradeep@agaramtech.com>
2021-02-18 13:20:51 +0530

    init

--- src/main/java/com/agaram/eln/primary/service/multitenant/DatasourceService.java
+++ src/main/java/com/agaram/eln/primary/service/multitenant/DatasourceService.java
@@ -102,12 +102,12 @@ public class DatasourceService {
 		
 		Tenantname.setName(Tenantname.getName().toLowerCase());
 		Tenantname.setDriverClassName(env.getProperty("app.datasource.eln.driverClassName"));
-		Tenantname.setUrl(gettenanturlDataBasename(Tenantname.getName()));
+		//Tenantname.setUrl(gettenanturlDataBasename(Tenantname.getName()));
 		Tenantname.setInitialize(true);
 		Tenantname.setUsername(env.getProperty("app.datasource.eln.username"));
 		Tenantname.setPassword(env.getProperty("app.datasource.eln.password"));
 		Tenantname.setArchivename(Tenantname.getName().toLowerCase()+"archive");
-		Tenantname.setArchiveurl(gettenanturlDataBasename(Tenantname.getName().toLowerCase()+"archive"));
+		///Tenantname.setArchiveurl(gettenanturlDataBasename(Tenantname.getName().toLowerCase()+"archive"));
 		objres.setStatus(true);
 		Tenantname.setObjResponse(objres);
 		
