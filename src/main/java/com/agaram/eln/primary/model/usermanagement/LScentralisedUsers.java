28738cd763b580a4a15dfef931eaa5f6084e415a
pradeepmanas <pradeep@agaramtech.com>
2021-03-22 14:02:53 +0530

    init

--- src/main/java/com/agaram/eln/primary/model/usermanagement/LScentralisedUsers.java
+++ src/main/java/com/agaram/eln/primary/model/usermanagement/LScentralisedUsers.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class LScentralisedUsers {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Basic(optional = false)
 	@Column(name = "centralisedusercode")
 	private int centralisedusercode;
